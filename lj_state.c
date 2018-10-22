e622e6ce5a33abbc83b145d31b2d1aa648f0cf61
Drecik <drecikxu@gmail.com>
2018-10-22 11:22:58 +0800

    1. 增加了线程upvalue的遍历 2. whitelist死循环 3. markold崩溃问题 4. correctgraylist case变量错误问题 5. gen参数初始化

--- lj_state.c
+++ lj_state.c
@@ -217,6 +217,8 @@ LUA_API lua_State *lua_newstate(lua_Alloc f, void *ud)
   g->gc.total = sizeof(GG_State);
   g->gc.pause = LUAI_GCPAUSE;
   g->gc.stepmul = LUAI_GCMUL;
+  g->gc.genminormul = LUAI_GENMINORMUL;
+  setgcparam(g->gc.genmajormul, LUAI_GENMAJORMUL);
   lj_dispatch_init((GG_State *)L);
   L->status = LUA_ERRERR+1;  /* Avoid touching the stack upon memory error. */
   if (lj_vm_cpcall(L, NULL, NULL, cpluaopen) != 0) {
